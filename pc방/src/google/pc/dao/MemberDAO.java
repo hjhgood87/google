168bdd49124966c9f22d0b03d45c5c897ba0719b
402A-1 <402A-1@hjh>
2015-12-08 17:00:12 +0900

    회원조회까지

--- pc방/src/google/pc/dao/MemberDAO.java
+++ pc방/src/google/pc/dao/MemberDAO.java
@@ -2,6 +2,7 @@ package google.pc.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import google.pc.model.MemCommand;
@@ -41,6 +42,47 @@ public class MemberDAO {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	public void memberList() {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String sql = null;
+		Context context = new Context();
+
+		try {
+			connection = context.basicDataSource.getConnection();
+
+			sql = "select *";
+			sql += " from MEMBER m ";
+
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				int num = resultSet.getInt("idx");
+				String id = resultSet.getString("id");
+				String name = resultSet.getString("name");
+				System.out.print("번호: " + num);
+				System.out.print(", 아이디= " + id);
+				System.out.print(", 이름= " + name);
+				System.out.println();
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				resultSet.close();
+				preparedStatement.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 }
--- pc방/src/google/pc/main/Client.java
+++ pc방/src/google/pc/main/Client.java
@@ -1,10 +1,9 @@
 package google.pc.main;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
+import google.pc.dao.MemberDAO;
 import google.pc.service.MemberJoin;
 
 public class Client {
@@ -24,11 +23,12 @@ public class Client {
 			try {
 				memberJoin.join();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			break;
 		case 2:
+			MemberDAO memberDAO = new MemberDAO();
+			memberDAO.memberList();
 
 			break;
 		}
