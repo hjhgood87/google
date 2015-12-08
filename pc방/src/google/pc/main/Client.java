534fc0ec498160b1d77e15333b508fe1110d3636
402A-1 <402A-1@hjh>
2015-12-08 16:50:35 +0900

    관리자 클라이언트

--- pc방/src/google/pc/main/Client.java
+++ pc방/src/google/pc/main/Client.java
@@ -1,5 +1,37 @@
 package google.pc.main;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+import google.pc.service.MemberJoin;
+
+public class Client {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("1.회원가입");
+		System.out.print("2.회원조회");
+
+		System.out.println("번호를 선택하세요.");
+		System.out.print("번호 입력: ");
+		int num = scanner.nextInt();
+
+		switch (num) {
+		case 1:
+			MemberJoin memberJoin = new MemberJoin();
+			try {
+				memberJoin.join();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case 2:
+
+			break;
+		}
+
+	}
 }
